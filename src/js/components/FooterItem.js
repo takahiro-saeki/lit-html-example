bee315fa6f177236fe5e92956f8cfc09c49fa469
Saeki, Takahiro <ts-takahiro.saeki@rakuten.com>
2017-12-21 14:19:16 +0900

    fixed: deleted directive. #9

--- src/js/components/FooterItem.js
+++ src/js/components/FooterItem.js
@@ -1,10 +1,10 @@
-import { html, directive } from 'lit-html';
+import { html } from 'lit-html';
 import filterChange from '../domain/filterChange';
 
 const FooterItem = filter => html`
   <li>
     <a href="#/${filter.type === 'All' ? '' : filter.type.toLowerCase()}" 
-      className="${directive(() => (filter.isSelected ? 'selected' : ''))}"
+      className="${filter.isSelected ? 'selected' : ''}"
       on-click="${e => filterChange(e)}">
       ${filter.type}
     </a>
--- src/js/components/TodoItem.js
+++ src/js/components/TodoItem.js
@@ -1,11 +1,9 @@
-import { html, directive } from 'lit-html';
+import { html } from 'lit-html';
 import deleteChild from 'domain/deleteChild';
 import check from 'domain/check';
 
 const TodoItem = item => html`
-  <li id="${item.id}" className="${directive(
-  () => (item.isChecked ? 'completed' : '')
-)}">
+  <li id="${item.id}" className="${item.isChecked ? 'completed' : ''}">
     <input class="toggle" type="checkbox" on-click="${e =>
       check(e)}" checked="${item.isChecked}">
     <label>${item.title}</label>
